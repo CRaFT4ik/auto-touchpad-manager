8ce4a634f40bea41bdfdc25faf54246c2361d1c8
CRaFT4ik <CRaFT4ik@gmail.com>
2018-08-05 01:55:45 +0300

    Increased time for sleep before touchpad disable after startup.

--- autosetup.sh
+++ autosetup.sh
@@ -12,7 +12,7 @@ RULES_PATH="/etc/udev/rules.d/touchpad.rules"
 STARTUP_PATH="/home/"$USERNAME"/.config/upstart/touchpad.conf"
 
 RULES_CONTENT=$(cat "touchpad.rules" | awk -v x=$USERNAME -v y=$SCRIPT_PATH '{ gsub("\$USER", x); gsub("\$PATH", y); print $0; }')
-STARTUP_CONTENT=$'start on startup\npre-start exec sleep 5\ntask\nexec /bin/bash '$SCRIPT_PATH'/script.sh'
+STARTUP_CONTENT=$'start on startup\npre-start exec sleep 10\ntask\nexec /bin/bash '$SCRIPT_PATH'/script.sh'
 
 if [[ -n "$1" && $1 = "-rm" ]]; then
 	sudo rm -f $RULES_PATH
